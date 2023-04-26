904edde48ccfb123c89b92a99f1a750a9a1c4738
Mercovsk <marcbaja08@gmail.com>
2023-04-26 15:27:53 +0800

    feat: created and migrated employee model

---
+++ employee/choices.py
@@ -0,0 +1,86 @@
+DEPT = (
+    (1, "Administration"),
+    (2, "Accounting Department"),
+    (3, "IT Department"),
+    (4, "HR Department"),
+)
+
+DIV = (
+    (1, "Administration"),
+    (2, "Accounting Department"),
+    (3, "IT Department"),
+    (4, "HR Department"),
+)
+
+POS = (
+    (1, "President"),
+    (2, "Vice President"),
+    (3, "Manager"),
+    (4, "Supervisor"),
+    (5, "Sales Representative"),
+    (6, "Customer Service Representative"),
+    (7, "Marketing Coordinator"),
+    (8, "Human Resource Coordinator"),
+    (9, "Accountant"),
+    (10, "Administrative Assistant"),
+    (11, "IT Specialist"),
+    (12, "Graphic Designer"),
+    (13, "Operations Manager"),
+    (14, "Project Manager"),
+    (15, "Production Coordinator"),
+    (16, "Quality Assurance Specialist"),
+    (17, "Procurement Specialist"),
+    (18, "Logistics Coordinator"),
+    (19, "Business Development Manager"),
+    (20, "Legal Counsel"),
+)
+
+RANK = (
+    (1, 'Junior Level'),
+    (2, 'Mid Level'),
+    (3, 'Senior Level')
+)
+
+CITY = (
+    (1, "Caloocan"),
+    (2, "Las Piñas"),
+    (3, "Makati"),
+    (4, "Malabon"),
+    (5, "Mandaluyong"),
+    (6, "Manila"),
+    (7, "Marikina"),
+    (8, "Muntinlupa"),
+    (9, "Navotas"),
+    (10, "Parañaque"),
+    (11, "Pasay"),
+    (12, "Pasig"),
+    (13, "Paterso"),
+    (14, "Quezon City"),
+    (15, "San Juan"),
+    (16, "Taguig"),
+    (17, "Valenzuela"),
+)
+
+PAYROLL = (
+    (1, 20),
+    (2, 25),
+    (3, 30),
+)
+
+TAX = (
+    (1,"0%"),
+    (2,"15%"),
+    (3,"20%"),
+)
+
+SSS = (
+    (1, "30%"),
+    (2, "50%"),
+    (3, "70%"),
+)
+
+PHILHEALTH = (
+    (1, "30%"),
+    (2, "50%"),
+    (3, "70%"),
+)
